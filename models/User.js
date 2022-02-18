c029a1316ebb3ce4980e1cb9f180a39dc54baecc
Charles-Nyabeze <charlesnnyabeze@gmail.com>
2022-02-18 00:37:01 -0500

    dcyt login

--- models/User.js
+++ models/User.js
@@ -1,8 +1,15 @@
+const req = require('express/lib/request');
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
+const bcrypt = require('bcrypt')
 
 // create our User model
-class User extends Model {}
+class User extends Model {
+  // set up method to run on instance data (per user) to check password
+  checkPassword(loginPw) {
+    return bcrypt.compareSync(loginPw, this.password);
+  }
+}
 
 // define table columns and configuration
 User.init(
@@ -45,6 +52,17 @@ User.init(
       }
     },
     {
+      hooks: {
+        async beforeCreate(newUserData) {
+          newUserData.password = await bcrypt.hash(newUserData.password, 10);
+          return newUserData;
+        },
+        async beforeUpdate(updatedUserData) {
+          updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
+          return updatedUserData;
+        }
+  
+      },
       sequelize,
       timestamps: false,
       freezeTableName: true,
