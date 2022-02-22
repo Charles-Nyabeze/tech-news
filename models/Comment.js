b8c14ae39984b26c329d72ef1ab3d11c8bb980ff
Charles-Nyabeze <charlesnnyabeze@gmail.com>
2022-02-22 02:31:12 -0500

    comment fix

--- models/Comment.js
+++ models/Comment.js
@@ -4,9 +4,29 @@ const sequelize = require('../config/connection');
 class Comment extends Model {}
 
 Comment.init(
-  {
-    // columns will go here
-  },
+    {
+        id: {
+          type: DataTypes.INTEGER,
+          primaryKey: true,
+          autoIncrement: true
+        },
+        user_id: {
+          type: DataTypes.INTEGER,
+          allowNull: false,
+          references: {
+            model: 'user',
+            key: 'id'
+          }
+        },
+        post_id: {
+          type: DataTypes.INTEGER,
+          allowNull: false,
+          references: {
+            model: 'post',
+            key: 'id'
+          }
+        }
+      },
   {
     sequelize,
     freezeTableName: true,
