42f378d31371b6ff282d6a4263247577cc47da87
Charles-Nyabeze <charlesnnyabeze@gmail.com>
2022-02-27 07:28:25 -0500

    dashboard

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const routes = require('./controllers');
 const sequelize = require('./config/connection');
 const path = require('path')
 const exphbs = require('express-handlebars');
-const helpers = require('./utils/helpers');
+const helpers = require('./utils/helper');
 
 
 const app = express();
